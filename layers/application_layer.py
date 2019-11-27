cf714e84e57850f3db9bdd657991c4c760a93e30
ilya-unr <ikuznetsov@nevada.unr.edu>
2019-11-26 17:09:53 -0800

    Application Layer can send DATA or ACK packets

--- layers/application_layer.py
+++ layers/application_layer.py
@@ -9,10 +9,12 @@ class ApplicationLayer(LayerBase):
     def __init__(self, node_data, layer_id, args):
         super(ApplicationLayer, self).__init__(node_data, layer_id, args)
         self.host_buffer = Queue()
+		self.msg_type = 0	# 0 - send, 1 - ack... Assume every application starts at send
 
 
     def process_receive(self, msg):
         print("Node has battery: %f" % (self.node_data.battery,))
+		self.check_type(msg)
         self.host_buffer.put(msg)
 
     def process_send(self, msg):
@@ -20,8 +22,14 @@ class ApplicationLayer(LayerBase):
 
     def create_data(self):
             seed(1)
-            return (9/5)*random()+32	# returns some random Fahrenheit value for data
+			if self.msg_type == 0:
+            	return (9/5)*random()+32	# returns some random Fahrenheit value for DATA
+			else:
+				return 1	# sample num used for ACK
 
     def print_data(self):
             print(self.host_buffer.get())
+
+	def check_type(self, msg):
+		self.msg_type = msg.get_type
 
